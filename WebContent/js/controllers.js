dff89e6275dc505193db9c4ede20968f07fed5e6
Abhranil Naha <abhranilnaha@gmail.com>
2015-11-15 13:07:33 -0800

    Added support for AngularJS UI.

---
+++ WebContent/js/controllers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.loadPhones({phoneId: 'phones'});
+    $scope.orderProp = 'age';
+    $scope.getUserDetails = function() {
+    	Phone.getUserDetails(function(user) {
+    		$scope.userName = user.name;
+    	});
+    };
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.loadPhone({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    };   
+  }]);
